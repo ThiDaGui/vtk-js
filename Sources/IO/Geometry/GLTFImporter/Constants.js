26962dd6dd7f27a36dc81003ce0bfb6d4d13bc83
Damien DIDIER <damien.didier@epita.fr>
2023-09-06 19:12:50 +0200

    feat(gltfimporter): create Constants file for GLTF importer
    Feature constants: accessor.component.type, accessor.type, bufferView.target

---
+++ Sources/IO/Geometry/GLTFImporter/Constants.js
@@ -0,0 +1,29 @@
+export const AccessorComponentTypes = {
+  BYTE: 5120,
+  UNSIGNED_BYTE: 5121,
+  SHORT: 5122,
+  UNSIGNED_SHORT: 5123,
+  UNSIGNED_INT: 5125,
+  FLOAT: 5126,
+};
+
+export const AccessorTypes = {
+  SCALAR: "SCALAR",
+  VEC2: "VEC2",
+  VEC3: "VEC3",
+  VEC4: "VEC4",
+  MAT2: "MAT2",
+  MAT3: "MAT3",
+  MAT4: "MAT4",
+};
+
+export const BufferViewTargets = {
+  ARRAY_BUFFER: 34962,
+  ELEMENT_ARRAY_BUFFER: 34963,
+};
+
+export default {
+  AccessorComponentTypes,
+  AccessorTypes,
+  BufferViewTargets,
+};
